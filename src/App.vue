1953bca966b7b150f38bccc113b96a2dccbb2344
bezko <mathieu@bezko.org>
2023-02-23 11:45:09 -0500

    tout marche!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>    
         <div id="rndlzr">
-            <div id="digits">
+            <div id="digits" :class="{ shake: paspareil, zoom: pareil }">
               <div id="digiti1" class="digit">{{ digit1 }}</div>
               <div id="digiti2" class="digit">{{ digit2 }}</div>
             </div>
@@ -24,6 +24,7 @@
 </template>
 
 <script>
+
 const axios = require("axios");
 
 export default {
@@ -33,34 +34,74 @@ export default {
     return {
       digits :[],
       digit1: null,
-      digit2: null
+      digit2: null,
+      pareil: false,
+      paspareil: false,
+      timeout_id : null
     }
   },
   methods:{
     generate()
     {
       axios.post('/digits').then((response) => {
-            let pair = response.data
+            let pair = response.data;
             this.digits.push(pair);
             this.digit1 = pair[0];
             this.digit2 = pair[1];
+            this.anim();
         })
     },
     clearList()
     {
       axios.delete('/digits').then((response) => {
-            this.digits = []
+            this.digits = [];
             this.digit1 = null;
             this.digit2 = null;
         })
+    },
+    anim()
+    {
+        if ( this.timeout_id > 0)
+        {
+          clearTimeout(this.timeout_id)
+          this.pareil = false;
+          this.paspareil = false;
+          this.timeout_id = null;
+        }
+
+        if (this.digit1 === this.digit2)
+        {
+          this.pareil = true
+          this.timeout_id = setTimeout(() => {
+            this.pareil = false
+            this.timeout_id =null;
+          }, 400)
+        }
+        else
+        {
+          this.paspareil = true
+          this.timeout_id = setTimeout(() => {
+            this.paspareil = false
+            this.timeout_id =null;
+          }, 400)
+        }
     }
   },
   created() 
   {   
     axios.get('/digits').then((response) => {
             this.digits = response.data;
+            if (this.digits.length>0)
+            {
+              let pair = this.digits.slice(-1)[0];
+              this.digit1 = pair[0];
+              this.digit2 = pair[1];
+              this.anim();
+            }
         })
   }
+  
+ 
 }
 </script>
 
@@ -101,5 +142,54 @@ export default {
 {
   padding:15px;
 }
+
+
+.shake {
+  animation: shake 0.4s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(4px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-8px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(8px, 0, 0);
+  }
+}
+
+
+.zoom
+{
+  animation: zoom 0.4s ease infinite;
+}
+
+@keyframes zoom{
+  0% {
+    transform: scale(1, 1);
+  }
+  50% {
+    transform: scale(1.1, 1.1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
+}
+
+
 
 </style>
